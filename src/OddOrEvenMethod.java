da73a99335e38c5cdf3dc5b5a96923e5714bec6b
nataliiachobu <mebel2008@yahoo.com>
2023-05-18 11:21:52 -0700

    created OddOrEven function.

--- src/OddOrEvenMethod.java
+++ src/OddOrEvenMethod.java
@@ -1,14 +1,14 @@
 public class OddOrEvenMethod {
     public static void main(String[] args) {
-        System.out.println(OddOrEven(197));
+        System.out.println(OddOrEven(1));
     }
-    public static String OddOrEven(int a){
-        String result="";
-        if(a%2==0){
-            result="Even";
-        }else{
-            result="Odd";
+
+    public static String OddOrEven(int a) {
+        if (a % 2 == 0) {
+            return  "Even";
+        } else {
+            return  "Odd";
         }
-        return result;
+
     }
 }
